e1a3615f8e123683b468ea38d3623d6e7a9db158
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-09-28 22:29:36 -0600

    more fixes

--- account_signup_extended/models/res_user.py
+++ account_signup_extended/models/res_user.py
@@ -22,18 +22,8 @@ class ResUsers(models.Model):
 			:param token: signup token (optional)
 			:return: (dbname, login, password) for the signed up user
 		"""
-		
+
         """Extend the signup method to include the phone number field"""
         if 'phone' in values:
             values['phone'] = values.get('phone')
         return super(ResUsers, self).signup(values, token)
-
-		# if token:
-		# 	partner = self.env['res.partner']._signup_retrieve_partner(token, check_validity=True, raise_exception=True)
-		# 	partner_user = partner.user_ids and partner.user_ids[0] or False
-		# 	if partner_user:
-		# 		values['wk_dob'] = values.get('wk_dob')
-		# else:	
-		# 	values['wk_dob'] = values.get('wk_dob')
-		# return super(ResUsers, self).signup(values, token)
-		
