0cb9b0eda5c10eec0db5954fed4d68f404ae2657
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-09-28 21:23:06 -0600

    Revert commit, and add models folder to acct_su_extended

---
+++ account_signup_extended/models/res_partner.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+# Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>:wink:
+# See LICENSE file for full copyright and licensing details.
+#################################################################################
+from odoo import api, fields, models, _
+import re
+from datetime import datetime,date
+from odoo.exceptions import ValidationError
+import logging
+_logger = logging.getLogger(__name__)
+
+# class ResPartner(models.Model):
+# 	_inherit = 'res.partner'
+
+# 	wk_dob = fields.Date( string='Date of Birth')
+
+# 	@api.constrains('wk_dob')
+# 	def age_cal(self):
+# 		for rec in self:	
+# 			if self.wk_dob > date.today():
+# 				raise ValidationError("DOB should not exceed the Current Date")
+					
