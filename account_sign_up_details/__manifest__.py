a6bb693318e66658fd7870d334a7a56a1f90292d
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-09-30 16:25:03 -0600

    forgot ref view

--- account_sign_up_details/__manifest__.py
+++ account_sign_up_details/__manifest__.py
@@ -12,7 +12,8 @@
   "author"               :  "FHS - G.S.S.",
   "depends"              :  ['auth_signup'],
   "data"                 :  [
-                             'views/account_details_template.xml', #'views/res_partner_view.xml',
+                             'views/account_details_template.xml',
+                             'views/res_partner_view.xml',
                             ],
   "images"               :  ['static/description/fhs.png'],
   "application"          :  True,
