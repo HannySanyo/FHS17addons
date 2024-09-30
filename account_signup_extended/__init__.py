5e94a38271cde75df64d444f255580f1aed9df95
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-09-29 20:57:55 -0600

    Revert "Revert "DOB test""
    This reverts commit 53535130ed4070ac348d42721ba1af32535d47e4.

--- account_signup_extended/__init__.py
+++ account_signup_extended/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #################################################################################
-# Website controller
+#
+# Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>:wink:
+# See LICENSE file for full copyright and licensing details.
 #################################################################################
 from . import models
 from . import controllers
--- account_signup_extended/controllers/__init__.py
+++ account_signup_extended/controllers/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 #################################################################################
-# File for controller
+#
+# Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>:wink:
+# See LICENSE file for full copyright and licensing details.
 #################################################################################
 from . import main
