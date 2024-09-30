86694d92eb838c4f1277b78e91f9bda076f114a0
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-09-30 14:06:42 -0600

    Help from Chet, does this work?

--- account_sign_up_details/models/res_partner.py
+++ account_sign_up_details/models/res_partner.py
@@ -4,19 +4,35 @@
 #                    G. Sanyo    09/29/2024    Creation
 #################################################################################
 from odoo import api, fields, models, _
-import re
-from datetime import datetime, date
 import logging
 
 _logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    
-    attachment_contractor = fields.Binary(string="If you are a contractor, please provide contractor license documentation:", attachment=True)
-    attachment_contractor_name = fields.Char(string='Contractor Attachment Name')
-    attachment_taxexempt = fields.Binary(string="If you are tax exempt, please provide proof of exemption documentation:", attachment=True)
-    attachment_taxexempt_name = fields.Char(string='Tax Exempt Attachment Name')
+
+    # Field for contractor attachment
+    attachment_contractor = fields.Binary(
+        string="Contractor License Document",
+        attachment=True,
+        help="If you are a contractor, please provide contractor license documentation."
+    )
+    attachment_contractor_name = fields.Char(
+        string='Contractor Attachment Name',
+        help="Name of the contractor attachment file."
+    )
+
+    # Field for tax exemption attachment
+    attachment_taxexempt = fields.Binary(
+        string="Tax Exempt Document",
+        attachment=True,
+        help="If you are tax exempt, please provide proof of exemption documentation."
+    )
+    attachment_taxexempt_name = fields.Char(
+        string='Tax Exempt Attachment Name',
+        help="Name of the tax exempt attachment file."
+    )
+
 
 	# @api.constrains('phone')
 	# def age_cal(self):
