7d50f85508f85e24c8cae9e293eb57642edf493e
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-10-02 11:30:57 -0600

    Come on work

--- account_sign_up_details/models/res_partner.py
+++ account_sign_up_details/models/res_partner.py
@@ -8,4 +8,27 @@ from odoo import models, fields, api
 class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
 
+    attachment = fields.Binary(string="Attachment", attachment=True)
+    attachment_name = fields.Char(string='Attachment Name')
+
+    @api.model
+    def create_attachment_record(self, vals):
+        # Create the record
+        record = super(ResPartnerInherit, self).create(vals)
+
+        # Create an attachment if the binary field has data
+        if vals.get('attachment'):
+
+            record.x_studio_contractor_doc = vals.get('attachment')
+            record.x_studio_contractor_doc_filename = vals.get('attachment_name')
+
+            self.env['ir.attachment'].create_attachment_record({
+                'name': vals.get('attachment_name'),
+                'type': 'binary',
+                'datas': vals.get('attachment'),
+                'res_model': self._name,
+                'res_id': record.id,
+            })
+            
+        return record
 
