18179297d5fa98f374327ccd6fe948dd26312170
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-10-02 14:29:44 -0600

    only fiscal pos

--- account_sign_up_details/controllers/main.py
+++ account_sign_up_details/controllers/main.py
@@ -21,10 +21,10 @@ class AuthSignupHomeInherit(AuthSignupHome):
         if values.get('password') != qcontext.get('confirm_password'):
             raise UserError(_("Passwords do not match; please retype them."))
         
-        if values.get('contractor_doc_name'):
-            contractor_filedata = base64.b64encode(values.get('contractor_doc').read())
-            contractor_filename = values.get('contractor_doc').filename
-            values.update({'x_studio_contractor_doc': contractor_filedata, 'x_studio_contractor_doc_filename': contractor_filename})
+        # if values.get('contractor_doc_name'):
+        #     contractor_filedata = base64.b64encode(values.get('contractor_doc').read())
+        #     contractor_filename = values.get('contractor_doc').filename
+        #     values.update({'x_studio_contractor_doc': contractor_filedata, 'x_studio_contractor_doc_filename': contractor_filename})
 
         if values.get('fiscal_pos_doc_name'):
             fiscal_pos_filedata = base64.b64encode(values.get('fiscal_pos_doc').read())
