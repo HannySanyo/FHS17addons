88201ed502b72bc3ce1f67e52ec17d43ab488f9e
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-10-02 20:37:27 -0600

    revert to working one

--- account_sign_up_details/models/res_partner.py
+++ account_sign_up_details/models/res_partner.py
@@ -8,7 +8,30 @@ from odoo import models, fields, api
 class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
 
-    contractor_doc = fields.Binary(string='Contractor Document', attachment=True)
-    contractor_doc_name = fields.Char(string='Contractor Document Name')
-    tax_exemption_doc = fields.Binary(string='Tax Exemption Document', attachment=True)
-    tax_exemption_doc_name = fields.Char(string='Tax Exemption Document Name')
+    contractor_doc = fields.Binary(string='Contractor Doc', attachment=True)
+    contractor_doc_name = fields.Char(string='Contractor Doc Name')
+    fiscal_pos_doc = fields.Binary(string='Fiscal Position', attachment=True)
+    fiscal_pos_doc_name = fields.Char(string='Fiscal Position Name')
+
+
+    @api.model
+    def create_attachment_record(self, vals):
+        # Create the record
+        record = super(ResPartnerInherit, self).create(vals)
+
+        # Create an attachment if the binary field has data
+        if vals.get('contractor_doc'):
+
+            record.x_studio_contractor_doc = vals.get('contractor_doc')
+            record.x_studio_contractor_doc_filename = vals.get('contractor_doc_name')
+
+            self.env['ir.attachment'].create_attachment_record({
+                'name': vals.get('contractor_doc_name'),
+                'type': 'binary',
+                'datas': vals.get('contractor_doc'),
+                'res_model': self._name,
+                'res_id': record.id,
+            })
+            
+        return record
+
